9c165078ab5b9756e28562e78954084b50d3ad12
Sumanjay <cyberboysumanjay@gmail.com>
2018-01-22 11:25:41 +0530

    OOPS Lab 3 Work
    Check Prime number or not, Working with Constructors and operators. Implemented product of 2 complex numbers.

---
+++ OOPS/primeNumber.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <math.h>
+
+class Calc{
+private:
+  int a;
+public:
+  int Prime(int x){
+    a=x;
+    int count=0,n=sqrt(a)+1;
+    for ( int i=2; i<n; i++) {
+      if (a%i==0) {
+        count++; 
+      }
+    }
+    return count;
+  }
+};
+
+int main(){
+  int x;
+  std::cout << "Roll No. 161210040" << '\n';
+  std::cout << "Enter the number" << '\n';
+  std::cin >> x;
+  Calc obj1;
+
+ int count=obj1.Prime(x);
+  if (count==0) {
+    std::cout << "Prime Number" << '\n';
+  }
+  else
+  std::cout << "Not a Prime Number" << '\n';
+
+}
