0d99cee176aea0bfc8f3d17ecee33739767d565c
Sumanjay <cyberboysumanjay@gmail.com>
2018-01-29 10:53:40 +0530

    Lab 4 Prog 1
    Converter Program

---
+++ OOPS/converter.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include <math.h>
+
+class Convert{
+private:
+  float length;
+public:
+
+  Convert(float len=0){
+      length=len;
+  }
+
+   friend float inchToCm(Convert obj1);
+   friend float cmToInch(Convert obj1);
+   friend float milesToKm(Convert obj1);
+   friend float kmToMiles(Convert obj1);
+};
+
+float inchToCm(Convert obj1){
+  return 2.54*obj1.length;
+}
+
+float cmToInch(Convert obj1) {
+ return obj1.length/2.54;
+}
+
+float milesToKm(Convert obj1){
+  return 1.6*obj1.length;
+}
+
+float kmToMiles(Convert obj1){
+  return obj1.length/1.6;
+}
+
+int main(){
+  std::cout << "Roll No. 161210040" << '\n';
+  float len;
+  std::cout << "Input value of length: " << '\n';
+  std::cin >> len;
+  Convert obj1(len);
+
+  std::cout << "Which conversion would you like to make: " << '\n';
+  std::cout << "1. Inch to Cm\t"<<"2. Cm to Inch\t" <<"3. Miles to Km\t"<<"4. Km to Miles\t""5. Exit" << '\n';
+
+  int choice;
+  std::cin >> choice;
+   switch (choice) {
+    case 1: std::cout << inchToCm(obj1) << '\n'; break;
+    case 2: std::cout << cmToInch(obj1) << '\n'; break;
+    case 3: std::cout << milesToKm(obj1)<< '\n'; break;
+    case 4: std::cout << kmToMiles(obj1)<< '\n'; break;
+    case 5: std::cout << "Wrong Choice! Exiting...." << '\n';break;
+   }
+  return 0;
+
+
+}
