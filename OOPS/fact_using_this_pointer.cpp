b3eaa5709adf81fd2453c86f7f5fa6513c75618e
Sumanjay <cyberboysumanjay@gmail.com>
2018-04-09 11:27:34 +0530

    Create fact_using_this_pointer.cpp

---
+++ OOPS/fact_using_this_pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class factorial
+{ int n;
+  public: 
+         void fact(int n)
+        {  int i=1,f=1;
+           this->n=n;
+           for(i=1;i<=n;i++)
+               f=f*i;
+           cout<<"Fact of "<<n<<" is "<<f<<endl;
+        }
+};
+
+int main()
+{   int n;
+    factorial f; 
+    cout<<"Roll No. 161210040\n";
+    cout<<"Enter number: ";
+    cin>>n;
+    f.fact(n);
+    return 0;
+}
